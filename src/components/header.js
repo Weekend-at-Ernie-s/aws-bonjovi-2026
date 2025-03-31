f6008cd1f9d8a5a10f88fe95c51fbcb43058229b
Laura Kinson-Evans <lbkinson@Lauras-MBP.lan>
2025-03-31 12:38:27 -0700

    header spcing

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ import coldplayImage from '../assets/hero-lockup-no-symbols.png'
 
 export default function Header() {
   return (
-    <div className="h-96 md:h-166.5 lg:h-225 bg-top bg-cover xl:bg-static-wide  flex flex-col items-center xl:bg-antique-white xl:bg-repeat-y" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div><img className="mt-6 mb-10 w-28 md:w-40 lg:w-52 xl:w-56" src={logoImage} alt="AWS logo" /></div>
+    <div className="bg-top bg-cover xl:bg-static-wide  flex flex-col items-center xl:bg-antique-white xl:bg-repeat-y" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div><img className="mt-6 mb-0 w-28 md:w-40 lg:w-52 xl:w-56" src={logoImage} alt="AWS logo" /></div>
       <div><img className="px-14 md:px-24 lg:px-32 my-20 md:my-24 lg:my-28" src={coldplayImage} alt="Coldplay displayed in rainbow-colored letters" /></div>
     </div>
   )
